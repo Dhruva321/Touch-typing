b781de97a8205226909dc653abe0ff1d04e71458
Dhruva321 <kvsurya.rohan@gmail.com>
2023-05-24 18:42:00 +0530

    init

---
+++ quotes.js
@@ -0,0 +1,23 @@
+const quotes = {
+  Easy: [
+    "A bird in the hand is worth two in the bush.",
+    "Caught between a rock and a hard place.",
+    "Closing the barn door after the horse escapes.",
+  ],
+  Medium: [
+    "Do one thing every day that scares you.",
+    "Failure is the condiment that gives success its flavor.",
+    "Whatever you are, be a good one.",
+  ],
+  Hard: [
+    "Wake up WITH determination a'int (see for yourself) and ; ",
+    "Learning never exhausts the mind.So keep learning forever.",
+    "{The only way to do}, great WORK is ; To love ain't What you Do."
+  ],
+  Ultrahard: [
+    "In 1599 (the trou'pe) Moved ;To a new 'venue' 1872, the Globe [Theatre], south of 'the' Thames River {in London,}",
+    "?Wake up with Deter'mination in 8039. Go to [dance] bed with 'satisfaction'.",
+    "It's Going to be 'HARD' in 4826, but Hard Does not mean [impossible];",
+    "The 'only' way 987  do great [work] is ? love 'what' you do."
+  ]
+}
